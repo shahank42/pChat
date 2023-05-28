10beb36b096dc42dc1bed8719702a7c382912c3b
Shounak Ghosh <crazykavlogs@gmail.com>
2023-05-29 02:22:29 +0530

    added a drawer

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { drawerStore } from '@skeletonlabs/skeleton';
+	import type { DrawerSettings } from '@skeletonlabs/skeleton';
+
+    import menuIconUrl from "../svg/hamburger-svgrepo-com.svg?url";
+
+    const drawerSettings: DrawerSettings = {
+	    id: 'example-3',
+	    // Provide your property overrides:
+	    bgDrawer: 'bg-surface-800 text-white border',
+	    width: 'w-[280px] md:w-[480px]',
+	    padding: 'p-4',
+	    rounded: 'rounded-xl',
+    };
+</script>
+
+<header class="w-full flex flex-row text-center border-b border-grey p-4 items-center justify-between">
+	<a href="/"class="" >pChat</a>
+	<button class="btn variant-filled-secondary" on:click={() => {
+        drawerStore.open(drawerSettings);
+    }}>
+        <img src={menuIconUrl} class="w-5" alt="menu" />
+    </button>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,13 +5,18 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-
+	
+	import Header from '../components/Header.svelte';
 	import { Toast } from '@skeletonlabs/skeleton';
+	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import type { DrawerSettings } from '@skeletonlabs/skeleton';
 </script>
 
 <div class="flex flex-col h-[100dvh] md:w-3/4 md:mx-auto overflow-hidden md:border-l md:border-r">
 	<Toast position="t" />
-    <header class="w-full text-center border-b border-grey p-4"><a href="/">pChat</a></header>
+	<Drawer position="right" />
+	
+    <Header />
 	<slot />
     <footer class="w-full text-center border-t border-grey p-1">
 		<p class="text-sm">made by shahank</p>
