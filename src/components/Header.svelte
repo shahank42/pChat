8b39ba24da28c6754527f4ac297afdf0eb081d5e
shahank42 <crazykavlogs@gmail.com>
2023-06-12 22:52:29 +0530

    lil more smiting done

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-    import { drawerStore } from '@skeletonlabs/skeleton';
-	import type { DrawerSettings } from '@skeletonlabs/skeleton';
 
-    import menuIconUrl from "../svg/hamburger-svgrepo-com.svg?url";
 </script>
 
 <header class="w-full flex flex-row text-center border-b border-grey p-4 items-center justify-between">
